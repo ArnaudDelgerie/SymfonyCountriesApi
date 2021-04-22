a38cdcdfccdfd07b7bb0fe2a475364a5811fe2aa
Arnaud <delgerie.arnaud@gmail.com>
2021-04-22 16:46:37 +0200

    create Language entity, create Language json file and add loadLanguages method to AppFixtures

--- api/src/DataFixtures/AppFixtures.php
+++ api/src/DataFixtures/AppFixtures.php
@@ -2,16 +2,39 @@
 
 namespace App\DataFixtures;
 
+use App\Entity\Language;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
+use Doctrine\ORM\EntityManagerInterface as EntityManager;
+use Symfony\Component\DependencyInjection\ContainerInterface;
 
 class AppFixtures extends Fixture
 {
+    private $container;
+    private $manager;
+
+    public function __construct(ContainerInterface $container = null, EntityManager $manager)
+    {
+        $this->container = $container;
+        $this->manager = $manager;
+    }
+
     public function load(ObjectManager $manager)
     {
-        // $product = new Product();
-        // $manager->persist($product);
+        $this->loadLanguages();
+    }
 
-        $manager->flush();
+    private function loadLanguages()
+    {
+        $languages = json_decode(file_get_contents($this->container->getParameter("json_dir") . "/Language.json"), true);
+
+        foreach ($languages as $data) {
+            $language = new Language();
+            $language->setName($data['name']);
+            $language->setCode($data['code']);
+            $this->manager->persist($language);
+        }
+
+        $this->manager->flush();
     }
 }
